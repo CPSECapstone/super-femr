5df5cab162017487ae1f530f0e13c17c2f1b7362
waldenhillegass <waldenhillegass@gmail.com>
2023-06-12 11:23:08 -0700

    Ok, actually moved

---
+++ installer/macOS-x64/darwin/Resources/uninstall.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+#Generate application uninstallers for macOS.
+
+#Parameters
+DATE=`date +%Y-%m-%d`
+TIME=`date +%H:%M:%S`
+LOG_PREFIX="[$DATE $TIME]"
+
+#Functions
+log_info() {
+    echo "${LOG_PREFIX}[INFO]" $1
+}
+
+log_warn() {
+    echo "${LOG_PREFIX}[WARN]" $1
+}
+
+log_error() {
+    echo "${LOG_PREFIX}[ERROR]" $1
+}
+
+#Check running user
+if (( $EUID != 0 )); then
+    echo "Please run as root."
+    exit
+fi
+
+echo "Welcome to Application Uninstaller"
+echo "The following packages will be REMOVED:"
+echo "  __PRODUCT__-__VERSION__"
+while true; do
+    read -p "Do you wish to continue [Y/n]?" answer
+    [[ $answer == "y" || $answer == "Y" || $answer == "" ]] && break
+    [[ $answer == "n" || $answer == "N" ]] && exit 0
+    echo "Please answer with 'y' or 'n'"
+done
+
+
+#Need to replace these with install preparation script
+VERSION=__VERSION__
+PRODUCT=__PRODUCT__
+
+echo "Application uninstalling process started"
+# remove link to shorcut file
+find "/usr/local/bin/" -name "__PRODUCT__-__VERSION__" | xargs rm
+if [ $? -eq 0 ]
+then
+  echo "[1/3] [DONE] Successfully deleted shortcut links"
+else
+  echo "[1/3] [ERROR] Could not delete shortcut links" >&2
+fi
+
+rm -f /Applications/fEMR
+rm -rf /var/__PRODUCT__/
+
